e0779b8730c55be9621b74edab192c75b3469982
Ferhan Akkan <ferhan.akkan@emlakjet.com>
2021-02-19 20:21:01 +0300

    Initial Commit

---
+++ ObjcBasics/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcBasics
+//
+//  Created by Ferhan Akkan on 19.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcBasics/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ObjcBasics
+//
+//  Created by Ferhan Akkan on 19.02.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
